776c872601f9470412b8ce3d7e29bb58ca006bc7
KRochaS <karinerocha.s@hotmail.com>
2023-10-18 21:08:18 -0300

    Theme, Home, Layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { theme } from '@/styles/theme';
+import { CacheProvider } from '@chakra-ui/next-js';
+import { ChakraProvider } from '@chakra-ui/react';
+
+  
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+        <body suppressHydrationWarning={true}>
+        <CacheProvider>
+          <ChakraProvider theme={theme}>
+            {children}
+          </ChakraProvider>
+        </CacheProvider>
+        </body>
+    );
+  }
---
+++ src/styles/theme.ts
@@ -0,0 +1,38 @@
+import { extendTheme } from '@chakra-ui/react';
+
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+    weight : ['400', '500', '600','700'],
+    subsets: ['latin']
+  });
+
+const fonts = {
+  heading: poppins.style.fontFamily,
+  body: poppins.style.fontFamily,
+};
+
+
+export const theme = extendTheme({
+    colors: {
+      blackAlpha: {
+        '50': '#F5F8FA',
+      },
+      teal: {
+        '650': '#47585B'
+      },
+      gray: {
+        '300': '#DADADA'
+      }
+    },
+    fonts,
+    styles: {
+      global: {
+        body: {
+          bg: 'blackAlpha.50',
+          color: 'teal.650',
+        },
+      },
+    },
+  });
+
