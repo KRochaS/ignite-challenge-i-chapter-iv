e06dc54d0723be4147b49c0ef7139b81027e6c15
KRochaS <karinerocha.s@hotmail.com>
2023-10-22 23:11:59 -0300

    Banner, theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,36 +3,39 @@ import { extendTheme } from '@chakra-ui/react';
 import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
-    weight : ['400', '500', '600','700'],
+    weight: ['400', '500', '600', '700'],
     subsets: ['latin']
-  });
+});
 
 const fonts = {
-  heading: poppins.style.fontFamily,
-  body: poppins.style.fontFamily,
+    heading: poppins.style.fontFamily,
+    body: poppins.style.fontFamily,
 };
 
 
 export const theme = extendTheme({
     colors: {
-      blackAlpha: {
-        '50': '#F5F8FA',
-      },
-      teal: {
-        '650': '#47585B'
-      },
-      gray: {
-        '300': '#DADADA'
-      }
+        blackAlpha: {
+            '50': '#F5F8FA',
+        },
+        teal: {
+            '650': '#47585B'
+        },
+        gray: {
+            '300': '#DADADA'
+        },
+        yellow: {
+            '200': '#FFBA08'
+        }
     },
     fonts,
     styles: {
-      global: {
-        body: {
-          bg: 'blackAlpha.50',
-          color: 'teal.650',
+        global: {
+            body: {
+                bg: 'blackAlpha.50',
+                color: 'teal.650',
+            },
         },
-      },
     },
-  });
+});
 
